616d9011edffc29f935baf9686d16e841145ed2b
divi134 <96563541+divi134@users.noreply.github.com>
2022-07-05 14:27:04 +0530

    m=int(input()) n=m c=0 while n:     c+=1     n=n//10 a=[0]*c s=i=d=0 n=m while n:     s=n%10     a[i]=s     i+=1     n=n//10 for i in range(c-1,-1,-1):     if a[i]==6:         a[i]=9         break for i in range(c-1,-1,-1):     print(a[i],end="")

---
+++ Maximum_69_Number.py
@@ -0,0 +1,20 @@
+m=int(input())
+n=m
+c=0
+while n:
+    c+=1
+    n=n//10
+a=[0]*c
+s=i=d=0
+n=m
+while n:
+    s=n%10
+    a[i]=s
+    i+=1
+    n=n//10
+for i in range(c-1,-1,-1):
+    if a[i]==6:
+        a[i]=9
+        break
+for i in range(c-1,-1,-1):
+    print(a[i],end="")
